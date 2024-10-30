701d7ef96d3bc434e262a4d197e7cf4c83b2734b
Dudu-Passoni <dudupassoni@gmail.com>
2024-10-30 11:36:23 -0300

    fix: bin path

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sudo mkdir /usr/local/bin/ezfetch
-sudo cp ./ezfetch /usr/local/bin/ezfetch
+sudo cp ./ezfetch /usr/local/bin/
 
 sudo mkdir /usr/local/share/ezfetch
 sudo cp -r ./logos /usr/local/share/ezfetch
@@ -9,14 +8,14 @@ sudo cp -r ./logos /usr/local/share/ezfetch
 
 # Check if ~/.bashrc or ~/.zshrc exists and add to PATH
 if [ -f "$HOME/.bashrc" ]; then
-	echo 'export PATH="$PATH:/usr/local/bin/ezfetch"' >> "$HOME/.bashrc"
+	echo 'export PATH="$PATH:/usr/local/bin"' >> "$HOME/.bashrc"
 	echo 'export PATH="$PATH:/usr/local/share/ezfetch"' >> "$HOME/.bashrc"
 	source $HOME/.bashrc
 	echo "PATH updated in .bashrc"
 fi
 
 if [ -f "$HOME/.zshrc" ]; then
-	echo 'export PATH="$PATH:/usr/local/bin/ezfetch"' >> "$HOME/.zshrc"
+	echo 'export PATH="$PATH:/usr/local/bin"' >> "$HOME/.zshrc"
 	echo 'export PATH="$PATH:/usr/local/share/ezfetch"' >> "$HOME/.zshrc"
 	source $HOME/.zshrc
 	echo "PATH updated in .zshrc"
